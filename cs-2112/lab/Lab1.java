9131a328b21966d8b8d8a3047840aa3ed9427b0e
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2024-09-01 00:44:25 +0000

    Some work

---
+++ cs-2112/lab/Lab1.java
@@ -0,0 +1,10 @@
+package lab;
+
+import java.util.Scanner; 
+
+public class Lab1 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+    }
+}
---
+++ cs-3410/c-pg/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int n1 = 0b1110;
+  int n2 = 0b0011;
+
+  int num = n1 - n2;
+
+  printf("num = %d\n", num);
+  return 0;
+}
